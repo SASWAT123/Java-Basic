ac8f790f5f3c8948cae475eb9cd9d479cc328d10
SASWAT123 <psaswat21@gmail.com>
2020-06-13 22:48:48 +0530

    array basics changes

--- JavaBasics/src/main/java/Arrays/InsertionSort.java
+++ JavaBasics/src/main/java/Arrays/InsertionSort.java
@@ -7,11 +7,13 @@ public class InsertionSort {
 		
 		//start the outer loop to keep track of elements
 		for(int i = 1; i < len; i++) {
-			int current = arr[i];  //current wil hold the current element
+			int current = arr[i];  //current will hold the current element
 			int j = i;
 			
 			//start inner loop to compare the elements
 			while(j > 0 && (arr[j-1] > current)) {
+				
+				//compare the current element with the previous elements which are greater than it
 				arr[j] = arr[j-1];
 				j--;
 			}
--- JavaBasics/src/main/java/Arrays/Main.java
+++ JavaBasics/src/main/java/Arrays/Main.java
@@ -1,35 +1,44 @@
 package Arrays;
 
+import java.util.Arrays;
+
 public class Main {
 
 	public static void main(String[] args) {
 		
-		GameEntry g1 = new GameEntry("Test1", 10);
-		GameEntry g2 = new GameEntry("Test2", 20);
-		GameEntry g3 = new GameEntry("Test3", 30);
-		GameEntry g4 = new GameEntry("Test4", 40);
-		GameEntry g5 = new GameEntry("Test5", 50);
-		
-		ScoreBoard scoreBoard = new ScoreBoard(4);
-		scoreBoard.add(g1);
-		scoreBoard.add(g2);
-		scoreBoard.add(g3);
-		scoreBoard.add(g4);
-		scoreBoard.add(g5);
-		
-		scoreBoard.remove(2);
-		
-		scoreBoard.displayBoard();
+//		GameEntry g1 = new GameEntry("Test1", 10);
+//		GameEntry g2 = new GameEntry("Test2", 20);
+//		GameEntry g3 = new GameEntry("Test3", 30);
+//		GameEntry g4 = new GameEntry("Test4", 40);
+//		GameEntry g5 = new GameEntry("Test5", 50);
+//		
+//		ScoreBoard scoreBoard = new ScoreBoard(4);
+//		scoreBoard.add(g1);
+//		scoreBoard.add(g2);
+//		scoreBoard.add(g3);
+//		scoreBoard.add(g4);
+//		scoreBoard.add(g5);
+//		
+//		scoreBoard.remove(2);
+//		
+//		scoreBoard.displayBoard();
 		
 		//Insertion Sort
-		InsertionSort is = new InsertionSort();
+//		InsertionSort is = new InsertionSort();
+//		
+//		int[] arr = {3,2,1,6,14,5};
+//		is.sort(arr);
+//		
+//		for(int i = 0; i < arr.length; i++) {
+//			System.out.println(arr[i]);
+//		}
 		
-		int[] arr = {3,2,1,6,14,5};
-		is.sort(arr);
+		//Array methods
+		int[] arr1 = new int [] {1, 2, 3, 4}; 
+        int[] arr2 = new int [] {1, 2, 3, 4}; 
 		
-		for(int i = 0; i < arr.length; i++) {
-			System.out.println(arr[i]);
-		}
+        System.out.println("is arr1 equals to arr2 : " + 
+                Arrays.equals(arr1, arr2)); 
 		
 	}
 
